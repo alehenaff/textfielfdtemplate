a39aafef286190c62d480b36b13b93996b01f842
Alain Le Hénaff <alain.lehenaff@meteo.fr>
2017-11-23 09:13:32 +0000

    code format

--- templatefield/tests.py
+++ templatefield/tests.py
@@ -6,23 +6,23 @@ from templatefield.filters import simplesubstitution
 class TextFieldTemplateTest(TestCase):
     def setUp(self):
         TextFieldTemplate.objects.create(template='Bonjour {{client.name}}, {{client.address}}',
-        context='{"client":{"name":"toto","address":"Paris"}}')
+                                         context='{"client":{"name":"toto","address":"Paris"}}')
         TextFieldTemplate.objects.create(template='{{domaine.name|simplesubstitution("aerodom")}}',
-        context='{"domaine":{"name":"atlantique"}}')
-        Substitution.objects.create(name='aerodom',key='atlantique',value='z')
+                                         context='{"domaine":{"name":"atlantique"}}')
+        Substitution.objects.create(name='aerodom', key='atlantique', value='z')
     
     def test_simple_render(self):
         a = TextFieldTemplate.objects.first()
-        self.assertEqual(a.rendertemplate(),'Bonjour toto, Paris')
+        self.assertEqual(a.rendertemplate(), 'Bonjour toto, Paris')
     
     def test_jinja_render(self):
         a = TextFieldTemplate.objects.first()
-        self.assertEqual(a.rendertemplatejinja(),'Bonjour toto, Paris')
+        self.assertEqual(a.rendertemplatejinja(), 'Bonjour toto, Paris')
 
     def test_undefined_variables(self):
         a = TextFieldTemplate.objects.first()
-        self.assertEqual(a.get_undefined_variables(),{'client'})
+        self.assertEqual(a.get_undefined_variables(), {'client'})
 
     def test_filter(self):
         a = TextFieldTemplate.objects.last()
-        self.assertEqual(a.rendertemplatejinja(filters=[('simplesubstitution',simplesubstitution)]),'z')
+        self.assertEqual(a.rendertemplatejinja(filters=[('simplesubstitution', simplesubstitution)]), 'z')
